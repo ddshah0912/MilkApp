e6d5f6e162c6136220387154078cef7483457d3b
Jiten1312 <jitendhimmar@gmail.com>
2020-08-07 22:19:17 +0530

    forth

---
+++ login/lib/pages/ForthPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class Forth extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Container(
+      child: new Center(
+        child: new Icon(Icons.person, size: 150.0, color: Colors.black),
+      )
+    );
+  }
+}
--- login/lib/pages/home.dart
+++ login/lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './FirstPage.dart' as first;
 import './SecondPage.dart' as second;
 import './ThirdPage.dart' as third;
+import './ForthPage.dart' as forth;
 
 class MyTabs extends StatefulWidget{
   @override
@@ -15,7 +16,7 @@ class MyTabState extends State<MyTabs> with SingleTickerProviderStateMixin{
   @override
   void initState(){
     super.initState();
-    controller = new TabController(length: 3, vsync: this);
+    controller = new TabController(length: 4, vsync: this);
   }
 
   @override
@@ -28,7 +29,7 @@ class MyTabState extends State<MyTabs> with SingleTickerProviderStateMixin{
   Widget build(BuildContext context){
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Home"),
+        title: new Text("MilInWay"),
       ),
       bottomNavigationBar: new Material(
         color: Colors.blueAccent,
@@ -38,6 +39,7 @@ class MyTabState extends State<MyTabs> with SingleTickerProviderStateMixin{
             new Tab(icon: new Icon(Icons.home)),
             new Tab(icon: new Icon(Icons.date_range)),
             new Tab(icon: new Icon(Icons.contact_phone)),
+            new Tab(icon: new Icon(Icons.person)),
           ]
         )
       ),
@@ -46,7 +48,8 @@ class MyTabState extends State<MyTabs> with SingleTickerProviderStateMixin{
         children: <Widget>[
           new first.First(),
           new second.Second(),
-          new third.Third()
+          new third.Third(),
+          new forth.Forth()
         ],
       )
     );
